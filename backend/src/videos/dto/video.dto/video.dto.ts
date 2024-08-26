570de079379cd451a80623d0986e76da497aa678
Jabari Holloway <jabari@jabariholloway.com>
2024-08-26 16:26:53 -0500

    Define Video DTO

--- backend/src/videos/dto/video.dto/video.dto.ts
+++ backend/src/videos/dto/video.dto/video.dto.ts
@@ -1,1 +1,12 @@
-export class VideoDto {}
+import { IsString, IsUrl } from "class-validator";
+
+export class VideoDto {
+    @IsString() 
+    name: String; 
+
+    @IsString() 
+    artist: String; 
+
+    @IsUrl()
+    url: String;
+}
