24994622efa7801a3fd15d15513331ca59b6c99a
Jabari Holloway <jabari@jabariholloway.com>
2024-08-26 16:32:10 -0500

    Create video service scaffolding

--- backend/src/videos/videos.module.ts
+++ backend/src/videos/videos.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { VideosController } from './videos/videos.controller';
+import { VideosService } from './videos/videos.service';
+import { VideoDto } from './dto/video.dto/video.dto';
 
 @Module({
-    controllers: [VideosController]
+    controllers: [VideosController],
+    providers: [VideosService]
 })
 export class VideosModule {
 
+    create(video: VideoDto): VideoDto {
+        return null;
+    }
+
+    fetch(): VideoDto[] {
+        return []
+    }
 }
